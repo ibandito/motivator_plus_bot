2228a80e0d2dfea61baae3e3e5d949f69b02a01f
ibandito <78941567+ibandito@users.noreply.github.com>
2025-07-08 07:15:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
-API_TOKEN = 'ВАШ_ТОКЕН_ТУТ'
+API_TOKEN = 7458160287:AAFn3FNZHGr8wQBTV-eKL9YmzeaDi-7Gm8Y
 ADMIN_ID = 217716970
 
 logging.basicConfig(level=logging.INFO)
